7ca71c0e635879949884a4332baa5bb4b3b1e981
m8524769 <m8524769@gmail.com>
2026-09-26 16:12:34 +0800

    chore(deps): upgrade vitest to v5 and drop the dead test setup

--- vite.config.js
+++ vite.config.js
@@ -63,7 +63,6 @@ export default defineConfig({
       '**/.{idea,git,cache,output,temp}/**',
       '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*'
     ],
-    setupFiles: ['./src/tests/setup.ts'],
     coverage: {
       exclude: ['src/mocks', '.svelte-kit', 'src/**/*.test.ts'],
       reporter: ['text', 'json', 'html', 'lcov']
